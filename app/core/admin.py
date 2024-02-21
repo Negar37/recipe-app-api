3f352a54f939410e050c168344bcaa68925f5ae6
Negar <negar.mashayekhi73@gmail.com>
2024-02-22 00:04:12 +0100

    add DRF, Swager, and some tests for user APIs.

--- app/core/admin.py
+++ app/core/admin.py
@@ -2,11 +2,10 @@
 Django admin customization.
 """
 
+from core import models
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import gettext_lazy as _
-
-from core import models
 
 
 class UserAdmin(BaseUserAdmin):
@@ -29,6 +28,23 @@ class UserAdmin(BaseUserAdmin):
         (_("Important dates"), {"fields": ("last_login",)}),
     )
     readonly_fields = ["last_login"]
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "email",
+                    "password1",
+                    "password2",
+                    "name",
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                ),
+            },
+        ),
+    )
 
 
 admin.site.register(models.User, UserAdmin)
